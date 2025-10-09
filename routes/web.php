50ccdb7e2f5c9e8aa41c0a78966f741988266a93
kierartillero123 <artillerokier3@gmail.com>
2025-10-09 14:54:31 +0800

    part 1

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use App\Models\Student;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/dashboard', [StudentController::class, 'index'])->name('dashboard');
+Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
